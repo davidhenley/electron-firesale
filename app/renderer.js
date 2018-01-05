86fcb8b8a09a69b01fbcc4ffaa5ac27a12984168
David Henley <henleydr@gmail.com>
2018-01-04 23:22:48 -0600

    Show edited

--- app/renderer.js
+++ app/renderer.js
@@ -17,6 +17,7 @@ const renderMarkdownToHtml = markdown => {
 
 markdownView.addEventListener('keyup', e => {
   renderMarkdownToHtml(e.target.value);
+  currentWindow.setDocumentEdited(true);
 });
 
 openFileButton.addEventListener('click', () => {
